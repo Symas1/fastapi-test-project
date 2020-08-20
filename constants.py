feb2d037e9ca10d07681161e0bd7b8c48f2215b2
oleksii <oleksii.symon@gmail.com>
2020-08-20 22:37:29 +0300

    updated constants

--- constants.py
+++ constants.py
@@ -2,5 +2,5 @@ import os
 
 from config.utils import load_conf
 
-ENVIRONMENT_TYPE = os.environ['ENVIRONMENT_TYPE']
+ENVIRONMENT_TYPE = os.environ.get('ENVIRONMENT_TYPE', 'base')
 CONF = load_conf(filepath=f'config/{ENVIRONMENT_TYPE}.yml')
